5114e8cbb102b2154f05bac2fa2af5a05562ff2f
Deepak <dpoo.bang@gmail.com>
2016-04-14 22:08:54 +0530

    adding bubble_sort_by method

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -3,7 +3,7 @@ def bubble_sort arr
 	else
 		sorted = true
 		while sorted #'while loop' starts here
-			sorted = false #'while loop' ends when if condition in line 8 is not met
+			sorted = false #'while loop' stops here after 'if' condition in line 8 is not met
 			(arr.size-1).times do |i|
 				if arr[i] > arr[i+1] #doesn't go to next line if condition is not met
 					arr[i], arr[i+1] = arr[i+1], arr[i]
@@ -15,7 +15,29 @@ def bubble_sort arr
 	end
 end
 
+array_numbers = [4,3,78,2,0,2] 
 
-array = [4,3,78,2,0,2] 
+p bubble_sort array_numbers #returns[0,2,2,3,4,78]
 
-p bubble_sort array #returns[0,2,2,3,4,78]
+
+#-----------------------------------------------------------------
+#bubble_sort_by
+
+def bubble_sort_by(array)
+	counter = array.size - 1
+	while counter > 0
+		array.each_index do |i|
+			value = yield array[i-1], array[i]
+			if value > 0
+				array[i-1], array[i] = array[i], array[i-1]
+			end
+		end
+		counter -= 1
+	end
+	p array
+end
+
+array_word = ['hi', 'hello', 'hey']
+bubble_sort_by array_word do |left,right|
+left.length - right.length
+end
